d904f6f54981cc5aa52fe7064a3752cc6131be0b
Milan Sahoo <milansahoo7211662005@gmail.com>
2025-02-09 18:35:26 +0530

    Update itemApi.js

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -5,15 +5,15 @@ export const fetchItems = async () => {
     return new Promise((resolve) => {
       setTimeout(() => {
         const items = [
-          { id: 1, name: 'T-shirt', price: '249', imageUrl: '/images/tshirt.png' },
-          { id: 2, name: 'Jeans', price: '599', imageUrl: '/images/jeans.png' },
-          { id: 3, name: 'Sneakers', price: '999', imageUrl: '/images/sneakers.png' },
-          { id: 4, name: 'Watches', price: '399', imageUrl: '/images/watches.png' },
+          { id: 1, name: 'T-shirt', price: '124.50', originalPrice: '249', discount: '50%', imageUrl: '/images/tshirt.png' }, 
+          { id: 2, name: 'Jeans', price: '299.50', originalPrice: '599', discount: '50%', imageUrl: '/images/jeans.png' },
+          { id: 3, name: 'Sneakers', price: '499.50', originalPrice: '999', discount: '50%', imageUrl: '/images/sneakers.png' },
+          { id: 4, name: 'Watches', price: '199.50', originalPrice: '399', discount: '50%', imageUrl: '/images/watches.png' },
           { 
             id: 5, 
             name: 'Formal-shirt', 
             price: 'Rs. 999.00', 
-            originalPrice: 'Rs. 1,999.00', // Store original price separately
+            originalPrice: 'Rs. 1,999.00', 
             discount: '50%', 
             imageUrl: 'https://myraymond.com/cdn/shop/files/RMSX11674-B8_1.jpg?v=1729593432&width=1445' 
           }
